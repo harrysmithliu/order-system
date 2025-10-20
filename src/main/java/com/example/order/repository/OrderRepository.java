b2b6ce0038c9fe834f6dd0afba8c2749e164c52c
harrysmithliu <harrysmithliu@gmail.com>
2025-10-20 00:25:19 -0400

    Exceptions; OrderRepository;

--- src/main/java/com/example/order/repository/OrderRepository.java
+++ src/main/java/com/example/order/repository/OrderRepository.java
@@ -1,7 +1,15 @@
 package com.example.order.repository;
 
+import com.example.order.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public class OrderRepository {
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findByOrderNo(String orderNo);
+    Page<Order> findByUserId(Long userId, Pageable pageable);
 }
