8f8e95cf2f498d15f0b818703739b969892bb4e2
harrysmithliu <harrysmithliu@gmail.com>
2025-10-23 23:23:59 -0400

    Add CORS for frontend access

--- src/main/java/com/harry/order/controller/OrderController.java
+++ src/main/java/com/harry/order/controller/OrderController.java
@@ -6,13 +6,11 @@ import com.harry.order.service.dto.OrderSummaryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
+@CrossOrigin(origins = "http://localhost:5173")     //允许跨域（CORS）
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
